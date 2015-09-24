cb1abc35f6c84048595c5bc5289e07197e49a541
Zane Sterling <zane.sterling@stonybrook.edu>
2015-09-24 17:08:24 -0400

    Add spinning cube

--- src/main.cpp
+++ src/main.cpp
@@ -13,16 +13,60 @@ bool initGlut(int argc, char **argv) {
 	glutInit(&argc, argv);
 
 	glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH);
-	glutInitWindowSize(600, 600);
+	glutInitWindowSize(WINDOW_WIDTH, WINDOW_HEIGHT);
 	glutCreateWindow("Avatar");
 
+	glutIdleFunc(idle);
 	glutDisplayFunc(display);
+	glutKeyboardFunc(keyboard);
+
+	glEnable(GL_CULL_FACE);
+	glEnableClientState(GL_VERTEX_ARRAY);
 
 	glClearColor(0., 0., 0., 1.);
 
 	return true;
 };
 
+void idle() {
+	rot_angle += 0.01;
+	if (rot_angle > 2 * M_PI) rot_angle -= 2 * M_PI;
+
+	glutPostRedisplay();
+}
+
 void display() {
-	printf("displaying\n");
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	// perspective transform
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluPerspective(FOV_Y, ASPECT_RATIO, 0.1, 100);
+
+	// model transform
+	glMatrixMode(GL_MODELVIEW);
+	glPushMatrix();
+	glTranslatef(0, 0, -8);
+	glRotatef(180. / M_PI * rot_angle, 0, 1, 0);
+
+	glVertexPointer(3, GL_FLOAT, 0, CUBE_TRIS);
+	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+	glColor3f(1, 1, 1);
+	glCullFace(GL_BACK);
+
+	glDrawArrays(GL_TRIANGLES, 0, 3 * 12);
+
+	glPopMatrix();
+
+	glutSwapBuffers();
+}
+
+void keyboard(unsigned char key, int x, int y) {
+	if (key == 'q') {
+		quit();
+	}
+}
+
+void quit() {
+	exit(EXIT_SUCCESS);
 }
--- src/main.h
+++ src/main.h
@@ -4,8 +4,21 @@
 #include <GL/freeglut.h>
 
 #include <stdio.h>
+#include <math.h>
+
+#include "cube.h"
+
+float FOV_Y = 30;
+int WINDOW_WIDTH = 800;
+int WINDOW_HEIGHT = 600;
+float ASPECT_RATIO = 1. * WINDOW_WIDTH / WINDOW_HEIGHT;
+
+float rot_angle = 0;
 
 int main(int argc, char **argv);
 bool initGlut(int argc, char **argv);
 
+void idle();
 void display();
+void keyboard(unsigned char key, int x, int y);
+void quit();
